25f2e918965401b3013a04135f4918af1e7b5d29
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-01-09 14:55:44 +0800

    chore: enhance `wait_for_it.sh` to eval multi commands.

--- bash/wait_for_it.sh
+++ bash/wait_for_it.sh
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 # This script is used to run a task when a specific task(<pid>) is finished.
 # Usage:
-#   ./wait_for_task.sh <pid> <command>
+#   ./wait_for_it.sh <pid> <command>
 # Example:
-#   ./wait_for_task.sh 1234 echo "Task 1234 is finished."
+#   ./wait_for_it.sh 1234 echo "Task 1234 is finished."
+#   ./wait_for_it.sh 1234 "cd /path/to/your/project && your_command"  # note the quotes
 
 pid=$1
 shift  # remove the first argument
@@ -14,4 +15,4 @@ while kill -0 $pid 2>/dev/null; do
     sleep 1
 done
 
-$cmd
+eval $cmd

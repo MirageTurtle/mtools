90d86742deca79c5061eba6ae3c781f04e193d96
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2026-04-16 10:43:36 +0800

    tg_bot_send(feat): update API base

--- bash/tg_bot_send.sh
+++ bash/tg_bot_send.sh
@@ -17,7 +17,7 @@ fi
 CHAT_ID="$1"
 MESSAGE="$2"
 
-curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+curl -s -X POST "https://mtelegra.mirageturtle.top/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
     -d chat_id="${CHAT_ID}" \
     -d text="${MESSAGE}" \
     -d parse_mode="Markdown"
